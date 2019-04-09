722663db6aa8a14b42ebfec33ca697eeac211a9d
Chris Gill <chrisfgill@live.com>
2019-04-09 08:11:42 -0400

    ignore support

--- package.ps1
+++ package.ps1
@@ -1,3 +1,3 @@
-electron-packager ./ cycletrainer --platform=win32 --arch=x64 --ignore=/client --overwrite
+electron-packager ./ cycletrainer --platform=win32 --arch=x64 --ignore=/client --ignore=/support --overwrite
 cp /client/build/ /cycletrainer-win32-x64/resources/app/
 cp /support/ /cycletrainer-win32-x64/
