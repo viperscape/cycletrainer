435b4d6f6028d4b91ff7c8d6176483fd07689f9d
Chris Gill <chrisfgill@live.com>
2019-04-09 09:34:45 -0400

    refactor metrics

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import { CalcSpeed } from "./metrics";
+import Metrics from "./metrics";
 import Settings from "./settings";
 
 const { ipcRenderer } = window.require('electron');
 
 class App extends Component {
-    constructor(props)
-    {
-        super(props);
-        this.state = {
-            power: null, speed: null, revs: null, 
-            bikespeed: 0, distance: 0 
-        };
-
-        let self = this;
-        ipcRenderer.on("data", function(event, data) {
-            let mass = 80; // kgs
-            let gradient = 0.0; // degrees
-            data.bikespeed = CalcSpeed(9.80665, data.power, mass, gradient);
-            let wheelsize = 668; // 700c
-            let distance = (data.revs * wheelsize * Math.PI) * 0.000001; // kilometers
-            distance = distance.toFixed(2);
-            if (data.revs > self.state.revs)
-                data.distance = self.state.distance + distance;
-            else 
-                data.distance = distance;
-
-            self.setState(data);
-        });
-    }
-
     render() {
         return (
             <div className="App">
-            {(this.state.power<0 || !this.state.power) && 
-                <div>
-                    Finding Trainer
-                </div>
-            }
-            <div>
-                <div>Power {this.state.power}</div>
-                <div>Cadence {this.state.speed}</div>
-                <div>Bike Speed {this.state.bikspeed}</div>
-                <div>Distance {this.state.distance}</div>
-            </div>
-
+            <Metrics bus={ipcRenderer}/>
             <Settings bus={ipcRenderer}/>
             </div>
         );
--- client/src/metrics.js
+++ client/src/metrics.js
@@ -1,3 +1,55 @@
+import React, { Component } from 'react';
+
+class Metrics extends Component {
+    constructor(props)
+    {
+		super(props);
+		
+        this.state = {
+            power: null, speed: null, revs: null, 
+            bikespeed: 0, distance: 0 
+		};
+		
+        let self = this;
+        this.props.bus.on("data", function(event, data) {
+            let mass = 80; // kgs
+            let gradient = 0.0; // degrees
+            data.bikespeed = CalcSpeed(9.80665, data.power, mass, gradient);
+            let wheelsize = 668; // 700c
+            let distance = (data.revs * wheelsize * Math.PI) * 0.000001; // kilometers
+            distance = distance.toFixed(2);
+            if (data.revs > self.state.revs)
+                data.distance = self.state.distance + distance;
+            else 
+                data.distance = distance;
+
+            self.setState(data);
+        });
+    }
+
+    render() {
+        return (
+			<div>
+				{(this.state.power<0 || !this.state.power) && 
+					<div>
+						Finding Trainer
+					</div>
+				}
+				<div>
+					<div>Power {this.state.power}</div>
+					<div>Cadence {this.state.speed}</div>
+					<div>Bike Speed {this.state.bikspeed}</div>
+					<div>Distance {this.state.distance}</div>
+				</div>
+			</div>
+        )
+    }
+}
+
+
+export default Metrics;
+
+
 // http://bikecalculator.com/veloUS.html
 function Newtonsmethod(aero, hw, tr, tran, p)
 {
@@ -31,5 +83,3 @@ function CalcSpeed(gravity, power, mass_person, gradient) {
 	let efficiency = 98 * 0.01;
 	return Newtonsmethod(air_r, wind, mass_r, efficiency, power); // in m/s
 }
-
-export { CalcSpeed };
